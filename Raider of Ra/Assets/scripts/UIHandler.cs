72ffa3fe7b745a05fc847cc6227eff72e1b4ac90
baqir <smbrizavi@gmail.com>
2023-05-29 02:16:16 +0500

    baqirs Ui

---
+++ Raider of Ra/Assets/scripts/UIHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler
+{
+    
+
+    void OnContinue() { }
+    void OnPlay() { }
+    void OnReplay() { }
+    void OnExit() { }
+    void OnControl() { }
+    void OnMainMenu() { }
+    void OnNextLevel() { }
+}
